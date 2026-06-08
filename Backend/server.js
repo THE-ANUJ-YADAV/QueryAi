ba9eaed8f954d6e3334c61177188bb6bbda084f7
Anuj Yadav <anuj882711@gmail.com>
2026-06-08 10:02:59 +0530

    Add nodeMailer

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import app from "./src/app.js"
+import connectToDB from "./src/config/database.js"
+
+
+const PORT = process.env.PORT || 3000
+
+connectToDB()
+    .catch((err)=>{
+        console.error("MongoDB connection failed: ",err)
+        process.exit(1)
+    })
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${3000}`)
+})
+
+
+
