e6a764cd5381b5cb70a40ef296a0c3f86b84cc73
Anuj Yadav <anuj882711@gmail.com>
2026-06-09 16:05:08 +0530

    Email verification and Integrate Gen Ai

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { registerValidator, loginValidator } from "../validators/auth.validator.js";
-import { register } from "../controllers/auth.controller.js";
+import { register, verifyEmail, login, getMe } from "../controllers/auth.controller.js";
+import { authUser } from "../middleware/auth.middleware.js";
 
 const authRouter = Router()
 
 authRouter.post("/register",registerValidator,register)
+
+authRouter.post('/login',loginValidator,login)
+
+authRouter.get('/get-me',authUser,getMe)
+
+authRouter.get('/verify-email', verifyEmail)
 
 export default authRouter;
---
+++ Backend/src/services/ai.services.js
@@ -0,0 +1,14 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { response } from "express";
+
+
+const model = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash-lite",
+    apiKey: process.env.GEMINI_API_KEY
+})
+
+export async function testAi(){
+    model.invoke("what is capital of MadhyaPradesh?").then((response)=>{
+        console.log(response.text)
+    })
+}
