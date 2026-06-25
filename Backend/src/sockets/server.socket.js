0d559c8c0a578ae72bef3e9e267196400e88188d
Anuj Yadav <anuj882711@gmail.com>
2026-06-26 02:05:31 +0530

    fix: update CORS origin to production frontend URL

--- Backend/src/sockets/server.socket.js
+++ Backend/src/sockets/server.socket.js
@@ -5,7 +5,7 @@ let io;
 export function initSocket(httpServer){
     io = new Server(httpServer,{
         cors:{
-            origin:"http://localhost:5173",
+            origin:"https://query-ai-frontend.vercel.app",
             credentials: true
         }
     })
