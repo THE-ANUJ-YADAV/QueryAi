08fc66a1096a8577b149cdada4a42bf79a083555
Anuj Yadav <anuj882711@gmail.com>
2026-06-26 02:30:07 +0530

    fix: correct cookie options for token in login response

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -100,11 +100,12 @@ export async function login(req, res) {
         username: user.username,
     }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
-    res.cookie("token", token,{
-        httpOnly: true,
-        secure: true,
-        samesize: "none",
-        maxage: 7*24*60*60*1000
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+
     })
 
     res.status(200).json({
